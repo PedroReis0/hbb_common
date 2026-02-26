268d4850138dcbb3c30346b76d468962829fe84f
PedroReis0 <pedroolivepbs@gmail.com>
2026-02-26 11:46:05 -0300

    Injeta IP do servidor e Key

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["49.12.243.233"];
+pub const RS_PUB_KEY: &str = "ZubCdhS8O4M+XW7k+T8r9Oix1YsTbQa7ArF4I5ImPWo=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
